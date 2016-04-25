8b8986870bbcd41ac1ce01e62de8b0bc95ae7cc7
Nikolay Zhidenko <dragg.ko@gmail.com>
2016-04-25 20:10:15 +0000

    Changed responsive design

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Credential;
 use Mail;
+use Illuminate\Support\Facades\Session;
 
 class MainController extends Controller
 {
@@ -36,22 +37,12 @@ class MainController extends Controller
     
     public function postSendEmail(Request $request)
     {
-        $data = [
-            'fname' =>  $request->input('fname'),
-            'lname' =>  $request->input('lname'),
-            'email' =>  $request->input('email'),
-            'phone' =>  $request->input('phone'),
-            'message' =>  $request->input('message'),
-        ];
-        
         $credential = Credential::first();
-        $to_email = $credential->email;
-        $to_name = $credential->name;
         
-//        Mail::send('message', ['data' => $data], function ($m) use ($to_email, $to_name) {
-//            $m->to($to_email, $to_name)->subject('Contact form from my demo site');
-//        });
-//        die();
-        return redirect()->to('contact')->withMessage('message', 'Your message has been sended!');
+        Mail::send('message', ['data' => $request->all()], function ($m) use ($credential) {
+            $m->to($credential->email, $credential->name)->subject('Contact form from my demo site');
+        });
+        Session::flash('message', 'Your message has been sended!');
+        return redirect()->to('contact');
     }
 }
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <title>Demp project</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.1/assets/owl.carousel.min.css">
@@ -9,6 +10,7 @@
         <link rel="stylesheet" type="text/css" href="{{url('css/main.css')}}">
         
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.js"></script>
+        
         <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.1/owl.carousel.min.js"></script>
         <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.min.js"></script>
         <script src="{{url('js/main.js')}}"></script>
@@ -17,22 +19,36 @@
         <div class="container">
             <!-- Static navbar -->
             <div class="row">
-            <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                    <div id="navbar" class="navbar-collapse collapse">
-                        <ul class="nav navbar-nav">
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
                             <a class="navbar-brand" href="/"><img src="{{url('images/logo2.PNG')}}"></a>
-                            <li><a href="automation">Automation</a></li>
-                            <li><a href="system-integration">System integration</a></li>
-                            <li><a href="consulting">Consulting</a></li>
-                            <li><a href="contact">Contact us</a></li>
-                        </ul>
-                    </div><!--/.nav-collapse -->
-                </div><!--/.container-fluid -->
-            </nav>
+                        </div>
+                        <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
+                            <ul class="nav navbar-nav">
+                                <li><a href="automation">Automation</a></li>
+                                <li><a href="system-integration">System integration</a></li>
+                                <li><a href="consulting">Consulting</a></li>
+                                <li><a href="contact">Contact us</a></li>
+                            </ul>
+<!--                            <ul class="nav navbar-nav navbar-right">
+                                <li class="active"><a href="./">Default <span class="sr-only">(current)</span></a></li>
+                                <li><a href="../navbar-static-top/">Static top</a></li>
+                                <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+                            </ul>-->
+                        </div><!--/.nav-collapse -->
+                    </div><!--/.container-fluid -->
+                </nav>
 
             @yield('content')
             </div>
         </div> <!-- /container -->
     </body>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </html>
